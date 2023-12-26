328b84a73a6e1f2d26ba623b92accacfedb4c6a1
SShipon <nxshipon@gmail.com>
2023-12-26 12:01:48 -0800

    Navbar created

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Footer from '../Sheared/Footer/Footer';
+import Navbar from '../Sheared/Navbar/Navbar';
 
 
 const Main = () => {
     return (
         <div>
+            <Navbar></Navbar>
             <Outlet></Outlet>
             <Footer></Footer>
         </div>
