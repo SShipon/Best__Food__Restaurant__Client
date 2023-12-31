9c09978be5c0ec78e2757fe42b280210ea456c12
SShipon <nxshipon@gmail.com>
2023-12-31 13:54:12 +0600

    navbar color change scroll

--- src/Sheared/Navbar/Navbar.jsx
+++ src/Sheared/Navbar/Navbar.jsx
@@ -38,7 +38,7 @@ const Navbar = () => {
     return (
     
         <div className='my-20'>
-            <div className={colorChange ? 'navbar scroll' : 'navbar  fixed top-0 z-10'}>
+            <div className={colorChange ? 'navbar scroll' : 'navbar bg-white  fixed'}>
   <div className="navbar-start">
     <div className="dropdown">
       <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
@@ -57,7 +57,7 @@ const Navbar = () => {
   </div>
   {/* navbar-end dropdown-end */}
   <div className="navbar-end">
-  <div className="dropdown dropdown-end">
+  <div className="dropdown dropdown-end ">
       <div tabIndex={0} role="button" className="btn btn-ghost btn-circle">
         <div className="indicator">
           <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" /></svg>
@@ -79,7 +79,7 @@ const Navbar = () => {
   <div className="navbar-end">
 
     <Link to='/login' className="btn btn-outline btn-primary mr-1">Login</Link>
-    <Link  to='/register' className="btn btn btn-outline btn-secondary">Sign Up Free</Link>
+    <Link  to='/register' className="btn btn btn-outline btn-secondary  hidden  pt-3 py-5 px-5 mx-0 md:block">Sign Up Free</Link>
   </div>
  
 </div>
