6311986471d280b89c97533825a2b91ea2248b46
SShipon <nxshipon@gmail.com>
2024-01-01 16:17:22 +0600

    login page updated

--- src/components/Home/FoodItems/Foods.jsx
+++ src/components/Home/FoodItems/Foods.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Foods = () => {
     return (
         <div>
-            
+            <h1>This food items all </h1>
         </div>
     );
 };
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import Banner from './Banner/Banner';
+import Foods from './FoodItems/Foods';
 
 const Home = () => {
     return (
         <div>
            <Banner></Banner>
-           
+            <Foods></Foods>
         </div>
     );
 };
