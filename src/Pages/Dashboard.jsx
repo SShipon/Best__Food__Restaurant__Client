af0618f52d6f3eb7cb83a619e802abe0eed860a5
SShipon <nxshipon@gmail.com>
2024-01-03 02:04:12 +0600

    dashboard updated

--- src/Pages/Dashboard.jsx
+++ src/Pages/Dashboard.jsx
@@ -8,6 +8,15 @@ const Dashboard = () => {
             <CustomButton bgColor="customGreen" outlineColor="border-red-500">
              Red Button
             </CustomButton>
+            <div class="grid grid-cols-3 gap-4">
+  <div class="...  border ">01</div>
+  <div class="...">02</div>
+  <div class="...">03</div>
+  <div class="col-span-5 ...  border ">04</div>
+  <div class="...">05</div>
+  <div class="...">06</div>
+  <div class="col-span-2 ...">07</div>
+</div>
         </div>
     );
 };
