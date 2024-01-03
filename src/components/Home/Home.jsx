f12dc0da9c7534db4ffc0e839076033e6e85c7d3
sabrina <sabrinaahmednitu@gmail.com>
2024-01-03 23:50:22 +0600

    swiper added

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import Banner from './Banner/Banner';
 import Foods from './FoodItems/Foods';
+import PopularCategory from './PopularCategory/PopularCategory';
 import Logos from './RestaurantLogo/AddLogos';
 
 const Home = () => {
     return (
         <div>
-           <Banner></Banner>
-            <Foods></Foods>
+            <Banner></Banner>
+            <PopularCategory></PopularCategory>
             <Logos></Logos>
            
         </div>
---
+++ src/components/Home/PopularCategory/PopularCategory.jsx
@@ -0,0 +1,108 @@
+import React, { useRef, useState } from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/free-mode';
+import 'swiper/css/pagination';
+import { FreeMode, Pagination } from 'swiper/modules';
+import './PopularCategory.css';
+
+const PopularCategory = () => {
+  return (
+    <div>
+      <div className="popular-gategory-container">
+        <h1>OUR POPULAR CATEGORY 🔥 </h1>
+        <section>
+          <Swiper
+            slidesPerView={2}
+            freeMode={true}
+            breakpoints={{
+              576: {
+                slidesPerView: 2,
+              },
+              768: {
+                slidesPerView: 3,
+              },
+              992: {
+                slidesPerView: 4,
+              },
+              1200: {
+                slidesPerView: 6,
+              },
+            }}
+            pagination={{
+              clickable: true,
+            }}
+            modules={[FreeMode, Pagination]}
+            className="mySwiper"
+          >
+            <SwiperSlide>
+              <a href="#">
+                <img
+                  src="https://html.themefax.com/restina/html/rtl/assets/images/category_img_2.jpg"
+                  alt=""
+                />
+                <p>Dessert</p>
+              </a>
+            </SwiperSlide>
+            <SwiperSlide>
+              <a href="#">
+                <img
+                  src="https://html.themefax.com/restina/html/rtl/assets/images/category_img_1.jpg"
+                  alt=""
+                />
+                <p>Pizza</p>
+              </a>
+            </SwiperSlide>
+            <SwiperSlide>
+              <a href="#">
+                <img
+                  src="https://html.themefax.com/restina/html/rtl/assets/images/category_img_3.jpg"
+                  alt=""
+                />
+                <p>BURGER</p>
+              </a>
+            </SwiperSlide>
+            <SwiperSlide>
+              <a href="#">
+                <img
+                  src="https://html.themefax.com/restina/html/rtl/assets/images/category_img_4.jpg"
+                  alt=""
+                />
+                <p>DRINKS</p>
+              </a>
+            </SwiperSlide>
+            <SwiperSlide>
+              <a href="#">
+                <img
+                  src="https://html.themefax.com/restina/html/rtl/assets/images/category_img_2.jpg"
+                  alt=""
+                />
+                <p>Dessert</p>
+              </a>
+            </SwiperSlide>
+            <SwiperSlide>
+              <a href="#">
+                <img
+                  src="https://html.themefax.com/restina/html/rtl/assets/images/category_img_3.jpg"
+                  alt=""
+                />
+                <p>BURGER</p>
+              </a>
+            </SwiperSlide>
+            <SwiperSlide>
+              <a href="#">
+                <img
+                  src="https://html.themefax.com/restina/html/rtl/assets/images/category_img_1.jpg"
+                  alt=""
+                />
+                <p>Pizza</p>
+              </a>
+            </SwiperSlide>
+          </Swiper>
+        </section>
+      </div>
+    </div>
+  );
+};
+
+export default PopularCategory;
