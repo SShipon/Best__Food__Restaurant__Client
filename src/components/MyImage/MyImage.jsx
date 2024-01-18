46f2ae396099c20f11bcc70683e5d2f8f9248b8b
sabrina <sabrinaahmednitu@gmail.com>
2024-01-19 00:57:16 +0600

    single page images updated

---
+++ src/components/MyImage/MyImage.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import './MyImage.css'
+const MyImage = ({ imgs }) => {
+  console.log(imgs);
+  return (
+    <div className='all-single-images'>
+      <div>
+        <img src={imgs[1].url} alt="" className="main-single-image" />
+      </div>
+      <div className="all4-image-flex">
+        {imgs.map((image, index) => {
+          return (
+            <figure>
+              <img src={image.url} alt="" className="all4-image" key={index} />
+            </figure>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default MyImage;
