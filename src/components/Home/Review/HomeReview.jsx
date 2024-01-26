2a8ea09372e118981766ce0f89efc8bb6de552ba
SShipon <nxshipon@gmail.com>
2024-01-26 11:51:01 +0600

    heay

---
+++ src/components/Home/Review/HomeReview.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import Rating from "react-rating";
+
+export default function HomeReview({ review }) {
+  const { name, email, photo, dateTime, rating, customerFeedbackReview } =
+    review;
+  return (
+    <section className="flex justify-center">
+    <div className="card w-96 mr-5 glass">
+        <div className="card-body">
+          <div className="flex items-center">
+            <figure>
+              <img className="rounded-full w-[70px]" src={photo} alt="" />
+            </figure>
+            <h2 className=" text-xl font-semibold px-4">{name}</h2>
+          </div>
+       
+       <Rating
+            className="d-block text-blue text-[10px]"
+            readonly
+            initialRating={rating}
+            fullSymbol="fas fa-star"
+            emptySymbol="far fa-star"
+          ></Rating>
+       
+          <p className="text-sm">{email}</p>
+          <p>{customerFeedbackReview}</p>
+          <div className="card-actions justify-end">
+            <p>{dateTime}</p>
+          </div>
+        </div>
+      </div>
+
+    </section>
+  );
+}
